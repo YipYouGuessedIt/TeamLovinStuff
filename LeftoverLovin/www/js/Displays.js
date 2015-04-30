0e853b845f7b91469f2dca7cdc86d0d6bf045fd5
Duran Cole <yipyouguessedit@gmail.com>
2015-04-30 22:33:26 +0200

    random stuff

--- LeftoverLovin/www/js/Displays.js
+++ LeftoverLovin/www/js/Displays.js
@@ -7,40 +7,79 @@ $(document).ready(function () {
 
 function GetAllRecipes()
 {
-	var recipes;
-	var i = 0;
-	$xml = $( $.parseXML("../XML/Recipe-main.xml"));
-	$xml.find("recipe").each(function()
-	{
-		//get all ingredients
-		var ingredients;
-		$(this).find("ingredients").each(function()
-		{
-			var ingredientTemp;
-			ingredientTemp.ingredient = $(this).find("ingredient").text();
-			ingredientTemp.quantity = $(this).find("quantity").text();
-			ingredients[i] = ingredientTemp;
-		});
+    var object;
+    var j;
+    $.ajax({
+        type: "GET",
+        url: "./XML/Recipe-main.xml",
+        dataType: "xml",
 
-		//get all preperation steps
-		var prep;
-		$(this).find("ingredients").each(function()
-		{
-			prep[i] = $(this).find("step").text();
-		});
+        success:function(xml){
+            
+            
+            $(xml).find("recipe").each(function () {
+                var ingredients;
+                $(this).find("ingredients").each(function () {
+                    var tempIngredient;
+                    tempIngredient.ingredient=$(this).find("ingredient").text();
+                    tempIngredient.quantity=$(this).find("quantity").text();
+                });
+                var i=0;
+                var prep;
+                $(this).find("ingredients").each(function () {
+                    prep[i]=$(this).find("step").text();
+                    i++;
+                });
 
-		//build objects
-		var object;
-		object.id = $(this).attr("id").text();
-		object.name = $(this).find("name").text();
-		object.image = $(this).find("imageLink").text();
-		object.description = $(this).find("description").text();
-		object.mealTime = $(this).find("mealTime").text();
-		object.ingredients = ingredients;
-		object.prep = prep;
-		object.rating = ($(this).find("name").text())/($(this).find("name").text());
-		recipes[i] = object;
-		i++;
+                object.id = $(this).attr("id").text();
+                object.name = $(this).find("name").text();
+                object.image = $(this).find("imageLink").text();
+                object.description = $(this).find("description").text();
+                object.mealTime = $(this).find("mealTime").text();
+                object.ingredients = ingredients;
+                object.ingredientCount = i;
+                object.prep = prep;
+                object.rating = $(this).find("score").text();
+                j++;
+            });
+
+            DisplayRecipes(object,j);
+        }
+    });
+	//var recipes;
+	//var i = 0;
+	//$xml = $( $.parseXML(../XML/Recipe-main.xml));
+	//$xml.find("recipe").each(function()
+	//{
+	//	//get all ingredients
+	//	var ingredients;
+	//	$(this).find("ingredients").each(function()
+	//	{
+	//		var ingredientTemp;
+	//		ingredientTemp.ingredient = $(this).find("ingredient").text();
+	//		ingredientTemp.quantity = $(this).find("quantity").text();
+	//		ingredients[i] = ingredientTemp;
+	//	});
+
+	//	//get all preperation steps
+	//	var prep;
+	//	$(this).find("ingredients").each(function()
+	//	{
+	//		prep[i] = $(this).find("step").text();
+	//	});
+
+	//	//build objects
+	//	var object;
+	//	object.id = $(this).attr("id").text();
+	//	object.name = $(this).find("name").text();
+	//	object.image = $(this).find("imageLink").text();
+	//	object.description = $(this).find("description").text();
+	//	object.mealTime = $(this).find("mealTime").text();
+	//	object.ingredients = ingredients;
+	//	object.prep = prep;
+	//	object.rating = ($(this).find("name").text())/($(this).find("name").text());
+	//	recipes[i] = object;
+	//	i++;
 	});
 	
 	//Call next function
