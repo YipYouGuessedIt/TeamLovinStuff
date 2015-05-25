5aaf88f8516255cba5eabf70d1e4d0cbaf6f61ef
CostaxCoffee <u12104592@tuks.co.za>
2015-05-25 07:58:17 +0200

    added stuff

--- Final work/Browser/Leftoverlovin.sql
+++ Final work/Browser/Leftoverlovin.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2015 at 08:25 PM
+-- Generation Time: May 24, 2015 at 06:05 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
 
@@ -28,18 +28,18 @@ USE `leftoverlovin`;
 -- Table structure for table `fav`
 --
 
+DROP TABLE IF EXISTS `fav`;
 CREATE TABLE IF NOT EXISTS `fav` (
 `ID` int(11) NOT NULL,
   `RecipeID` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `fav`
 --
 
 INSERT INTO `fav` (`ID`, `RecipeID`) VALUES
-(2, 2),
-(4, 1);
+(2, 2);
 
 -- --------------------------------------------------------
 
@@ -47,12 +47,13 @@ INSERT INTO `fav` (`ID`, `RecipeID`) VALUES
 -- Table structure for table `ingredients`
 --
 
+DROP TABLE IF EXISTS `ingredients`;
 CREATE TABLE IF NOT EXISTS `ingredients` (
 `ID` int(11) NOT NULL,
   `Name` varchar(500) NOT NULL,
   `Qtype` varchar(25) NOT NULL,
   `Quantity` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `ingredients`
@@ -69,7 +70,12 @@ INSERT INTO `ingredients` (`ID`, `Name`, `Qtype`, `Quantity`) VALUES
 (9, 'Salami', 'Grams', 300),
 (12, 'oil', 'Millilitres', 100),
 (15, 'Mutton', 'Grams', 100),
-(16, 'Jam', 'Grams', 1000);
+(16, 'Jam', 'Grams', 1000),
+(18, 'Prawn', 'Grams', 1000),
+(19, 'Bread', 'Grams', 700),
+(20, 'Curry', 'Grams', 699),
+(21, 'Milk', 'Millilitres', 10000),
+(22, 'Chicken', 'Grams', 800);
 
 -- --------------------------------------------------------
 
@@ -77,6 +83,7 @@ INSERT INTO `ingredients` (`ID`, `Name`, `Qtype`, `Quantity`) VALUES
 -- Table structure for table `recipes`
 --
 
+DROP TABLE IF EXISTS `recipes`;
 CREATE TABLE IF NOT EXISTS `recipes` (
 `ID` int(11) NOT NULL,
   `Name` varchar(500) NOT NULL,
@@ -87,7 +94,7 @@ CREATE TABLE IF NOT EXISTS `recipes` (
   `score` int(11) NOT NULL,
   `votes` int(11) NOT NULL,
   `ImageLink` varchar(500) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `recipes`
@@ -95,7 +102,8 @@ CREATE TABLE IF NOT EXISTS `recipes` (
 
 INSERT INTO `recipes` (`ID`, `Name`, `Description`, `MealTime`, `Ingredients`, `Prep`, `score`, `votes`, `ImageLink`) VALUES
 (1, 'Grilled Chicken and Pasta Salad', 'Seasoned chicken strips highlight this pasta salad tossed with tomatoes, chopped lettuce, mozzarella, and onion. Feel free to add your favorite garnishes', 'Supper', 'Chicken$Grams$200,Pasta$Grams$200,Salad$Grams$200', 'Cook Chicken and pasta,add salad,mix', 192, 60, '874033.jpg'),
-(2, 'Tasty Mutton, Pea and Potato Curry', 'This is a quick and easy way to make a mild curry for family and friends.', 'Breakfast', 'Onion$Grams$100,Oil$Milliliters$100,CurryPowder$Grams$100,Mutton$Grams$100,Pea$Grams$100,Potato$Grams$100', 'catch lamb bunnies,Laugh at the funnies,Eat the muttons!', 249, 20, 'Tasty Mutton_30_1.1.131_326X580_30_1.1.131_326X580.Jpeg');
+(2, 'Tasty Mutton, Pea and Potato Curry', 'This is a quick and easy way to make a mild curry for family and friends.', 'Breakfast', 'Onion$Grams$100,Oil$Milliliters$100,CurryPowder$Grams$100,Mutton$Grams$100,Pea$Grams$100,Potato$Grams$100', 'catch lamb bunnies,Laugh at the funnies,Eat the muttons!', 249, 20, 'Tasty Mutton_30_1.1.131_326X580_30_1.1.131_326X580.Jpeg'),
+(3, 'Bunny Chow Bun Bun', 'Modern twist on the classic Bunny Chow', 'Anytime', 'Prawn$Grams$200,Bread$Grams$100,Curry$Grams$200', 'CooK prawns,add curry,mix well,add to bread', 60, 40, 'Gourmet Prawn Bunny Chows_30_1.1.832_326X580.Jpeg');
 
 --
 -- Indexes for dumped tables
@@ -127,17 +135,17 @@ ALTER TABLE `recipes`
 -- AUTO_INCREMENT for table `fav`
 --
 ALTER TABLE `fav`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `ingredients`
 --
 ALTER TABLE `ingredients`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
 --
 -- AUTO_INCREMENT for table `recipes`
 --
 ALTER TABLE `recipes`
-MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
